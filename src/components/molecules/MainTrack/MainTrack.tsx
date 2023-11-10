a385abd7b9c31f42c316375d901411e0024d344f
Joseff361 <Joseff361@outlook.com>
2023-11-09 20:49:15 -0500

    Finsih main track interface

--- src/components/molecules/MainTrack/MainTrack.tsx
+++ src/components/molecules/MainTrack/MainTrack.tsx
@@ -1,7 +1,40 @@
+import { useAppSelector } from '../../../store/hooks';
 import classes from './MainTrack.module.css';
 
 function MainTrack() {
-  return <div className={classes['main-track__container']}>MainTrack</div>;
+  const mainTrack = useAppSelector(state => state.tracks.currentTrack);
+  console.log(mainTrack);
+
+  return (
+    <div className={classes['main-track__container']}>
+      <img
+        className={classes['main-track__image']}
+        src={mainTrack?.album.cover_big}
+        alt="Track image"
+      />
+      <div className={classes['main-track__content']}>
+        <div className={classes['main-track__title']}>{mainTrack?.title}</div>
+        <div className={classes['main-track__subtitle']}>
+          by {mainTrack?.artist.name}
+        </div>
+        <div className={classes['main-track__subtitle']}>
+          {mainTrack?.album.title}
+        </div>
+        <div>
+          <button
+            className={`${classes['main-track__button']} ${classes['main-track__button--play']}`}
+          >
+            Play
+          </button>
+          <button
+            className={`${classes['main-track__button']} ${classes['main-track__button--follow']}`}
+          >
+            Follow
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MainTrack;
--- src/components/molecules/Playlist/Playlist.tsx
+++ src/components/molecules/Playlist/Playlist.tsx
@@ -6,10 +6,10 @@ function Playlist() {
   const tracks = useAppSelector(state => state.tracks.trackResultList);
   return (
     <ul className={classes['playlist__container']}>
-      {tracks.map((track, index) => (
+      {tracks.slice(1).map((track, index) => (
         <TrackMinItem
           key={track.id}
-          order={index}
+          order={index + 1}
           imageSource={track.album.cover_medium}
           trackName={track.title}
           trackArtist={track.artist.name}
